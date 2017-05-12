5480a0116f1bb2e80d2b7393c7ce6c119d0826c2
Robin Edbom <robin.edbom@gmail.com>
2017-05-13 00:06:29 +0200

    FUCK THIS AND FUCK EF!... Using a text file and Json tommorow.

--- FIFTTOW/MainActivity.cs
+++ FIFTTOW/MainActivity.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Android;
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.Widget;
 using Android.OS;
@@ -9,7 +11,6 @@ using Android.Util;
 using FIFTTOW.Servicies;
 using Android.Locations;
 using Android.Views;
-
 
 namespace FIFTTOW
 {
@@ -25,7 +26,7 @@ namespace FIFTTOW
             Manifest.Permission.AccessCoarseLocation,
         };
 
-        protected override void OnCreate(Bundle bundle)
+        protected override async void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
@@ -37,18 +38,55 @@ namespace FIFTTOW
             Button button = FindViewById<Button>(Resource.Id.MyButton);
 
 
-            button.Click += MyMethod;
 
-            if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) == Permission.Granted)
-            {
-                Console.WriteLine("asd");
-            }
-            else
-            {
-                Console.WriteLine("q654645");
-                RequestPermissions(PERMISSIONS_LOCATION,REQUEST_LOCATION);
-            }
+//            var dbFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+//            var fileName = "WifiLocations.db";
+//            var dbFullPath = Path.Combine(dbFolder, fileName);
+//            try
+//            {
+//                using (var db = new WifiLocationContext(dbFullPath))
+//                {
+//                    await db.Database.MigrateAsync(); //We need to ensure the latest Migration was added. This is different than EnsureDatabaseCreated.
+//
+//                    var location1= new WifiLocation() { Id = 1, Name = "Home"};
+//                    var location2= new WifiLocation() { Id = 2, Name = "Work"};
+//                    var location3= new WifiLocation() { Id = 3, Name = "Outside"};
+//
+//                    var catsInTheHat = new List<WifiLocation>() { location1,location2,location3};
+//
+//                    if(await db.WifiLocations.CountAsync() < 3)
+//                    {
+//                        await db.WifiLocations.AddRangeAsync(catsInTheHat);
+//                        await db.SaveChangesAsync();
+//                    }
+//
+//                    var wifiLocations = await db.WifiLocations.ToListAsync();
+//
+//                    foreach(var wifiLoc in wifiLocations)
+//                    {
+//                        Log.Debug("DEBUG", $"{wifiLoc.Id}, {wifiLoc.Name} ");
+//                    }
+//                }
+//
+//            }
+//            catch (Exception ex)
+//            {
+//                System.Diagnostics.Debug.WriteLine(ex.ToString());
+//            }
+
+//            button.Click += MyMethod;
+//
+//            if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) == Permission.Granted)
+//            {
+//                Console.WriteLine("asd");
+//            }
+//            else
+//            {
+//                Console.WriteLine("q654645");
+//                RequestPermissions(PERMISSIONS_LOCATION,REQUEST_LOCATION);
+//            }
         }
+
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
         {
@@ -83,6 +121,5 @@ namespace FIFTTOW
         }
 
     }
-
 
 }
